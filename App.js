0d8715db78526374258cc097217abee8ace2b603
Yvandlo <44481260+yvandlo@users.noreply.github.com>
2024-04-02 18:23:08 -0400

    axios set up

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5.js";
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 Lab5(app);
 Hello(app);
 app.listen(4000);
