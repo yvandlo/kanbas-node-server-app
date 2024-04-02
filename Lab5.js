2c3b0289005d0f234d95e0c977094aca506b9d39
Yvandlo <44481260+yvandlo@users.noreply.github.com>
2024-04-02 16:44:40 -0400

    Start of Lab 5

---
+++ Lab5.js
@@ -0,0 +1,97 @@
+const assignment = {
+    id: 1, title: "NodeJS Assignment",
+    description: "Create a NodeJS server with ExpressJS",
+    due: "2021-10-10", completed: false, score: 0,
+};
+const module = {
+    id: "M101",
+    name: "Full stack development",
+    description: "Full stack is the development to develop",
+    course: "CS1234"
+}
+
+
+const Lab5 = (app) => {
+    app.get("/a5/module", (req, res) => {
+        res.json(module);
+    });
+    app.get("/a5/module/name", (req, res) => {
+        res.json(module.name);
+    });
+
+    app.get("/a5/assignment", (req, res) => {
+        res.json(assignment);
+    });
+    app.get("/a5/assignment/title", (req, res) => {
+        res.json(assignment.title);
+    });
+    app.get("/a5/assignment/title/:newTitle", (req, res) => {
+        const { newTitle } = req.params;
+        assignment.title = newTitle;
+        res.json(assignment);
+    });
+    app.get("/a5/assignment/score", (req, res) => {
+        res.json(assignment.score);
+    });
+    app.get("/a5/assignment/score/:newScore", (req, res) => {
+        const { newScore } = req.params;
+        assignment.score = parseInt(newScore);
+        res.json(assignment);
+    });
+    app.get("/a5/assignment/completed", (req, res) => {
+        res.json(assignment.title);
+    });
+    app.get("/a5/assignment/completed/:newCompleted", (req, res) => {
+        const { newCompleted } = req.params;
+        assignment.completed = newCompleted;
+        res.json(assignment);
+    });
+
+
+    app.get("/a5/welcome", (req, res) => {
+        res.send("Welcome to Assignment 5");
+    });
+    app.get("/a5/add/:a/:b", (req, res) => {
+        const { a, b } = req.params;
+        const sum = parseInt(a) + parseInt(b);
+        res.send(sum.toString());
+    });
+    app.get("/a5/subtract/:a/:b", (req, res) => {
+        const { a, b } = req.params;
+        const sum = parseInt(a) - parseInt(b);
+        res.send(sum.toString());
+    });
+    app.get("/a5/calculator", (req, res) => {
+        const { a, b, operation } = req.query;
+        let result = 0;
+        switch (operation) {
+            case "add":
+                result = parseInt(a) + parseInt(b);
+                break;
+            case "subtract":
+                result = parseInt(a) - parseInt(b);
+                break;
+            case "multiply":
+                result = parseInt(a) * parseInt(b);
+                break;
+            case "divide":
+                result = parseInt(a) / parseInt(b);
+                break;
+            default:
+                result = "Invalid operation";
+        }
+        res.send(result.toString());
+    });
+
+    app.get("/a5/multiply/:a/:b", (req, res) => {
+        const { a, b } = req.params;
+        const sum = parseInt(a) * parseInt(b);
+        res.send(sum.toString());
+    });
+    app.get("/a5/divide/:a/:b", (req, res) => {
+        const { a, b } = req.params;
+        const sum = parseInt(a) / parseInt(b);
+        res.send(sum.toString());
+    });
+};
+export default Lab5;
