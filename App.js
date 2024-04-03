ee34defd3ac62f8b96888f33d231e91522b65d65
Yvandlo <44481260+yvandlo@users.noreply.github.com>
2024-04-02 21:00:17 -0400

    modules update 9

--- App.js
+++ App.js
@@ -3,10 +3,12 @@ import cors from 'cors';
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5.js";
 import CourseRoutes from "./Kanbas/courses/routes.js";
+import ModuleRoutes from "./Kanbas/Modules/routes.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+ModuleRoutes(app);
 CourseRoutes(app);
 Lab5(app);
 Hello(app);
