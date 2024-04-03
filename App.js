df8a4cffdb70aaa63ac90df162d331e4752ffd24
Yvandlo <44481260+yvandlo@users.noreply.github.com>
2024-04-03 14:37:39 -0400

    updated route

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5.js";
 import CourseRoutes from "./Kanbas/courses/routes.js";
-import ModuleRoutes from "./Kanbas/Modules/routes.js";
+import ModuleRoutes from "./Kanbas/modules/routes.js";
 
 const app = express();
 app.use(cors());
@@ -12,4 +12,4 @@ ModuleRoutes(app);
 CourseRoutes(app);
 Lab5(app);
 Hello(app);
-app.listen(4000);
+app.listen(process.env.PORT || 4000);
